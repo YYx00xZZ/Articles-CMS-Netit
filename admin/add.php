23da6c17d27e89dc19cacbce4214776fbacb2e59
YYx00xZZ <YYx00xZZ@protonmail.com>
2019-04-01 11:15:35 +0300

    Change in CDN and navigation
    Change CDN links with static
    Add Home button in navigation in add.php & delete.php

--- admin/add.php
+++ admin/add.php
@@ -40,13 +40,16 @@ if (isset($_SESSION['logged_in'])) {
     <body class="mt-5">
     <div class="container">
         <div class="row">
-            <div class="col-2">
+            <div class="col-xs-5 col-sm-5 col-md-3 col-lg-2">
                 <img class="mx-auto d-block p-1 rounded-circle" src="https://via.placeholder.com/150" />
                 <hr />
                 <p class="text-center"><?php echo $_SESSION['logged_in_username']; ?></p>
                 <hr />
 
                 <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="add.php">Add Article</a>
                     </li>
@@ -62,7 +65,7 @@ if (isset($_SESSION['logged_in'])) {
                 </ul>
             </div>
 
-            <div class="col-10">
+            <div class="col-xs-7 col-sm-7 col-md-9 col-lg-10">
                 <a href="index.php" id="logo">CMS</a>
 
                 <h4>Add Article</h4>
@@ -82,9 +85,8 @@ if (isset($_SESSION['logged_in'])) {
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="../assets/js/jquery-3.3.1.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
     </body>
     </html>
 <?php
--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['logged_in'])) {
         <body class="mt-5">
             <div class="container">
                 <div class="row">
-                    <div class="col-2">
+                    <div class="col-xs-5 col-sm-5 col-md-3 col-lg-2">
                         <img class="mx-auto d-block p-1 rounded-circle" src="https://via.placeholder.com/150" />
                         <hr />
                         <p class="text-center"><?php echo $_SESSION['logged_in_username']; ?></p>
@@ -48,7 +48,7 @@ if (isset($_SESSION['logged_in'])) {
                         </ul>
                     </div>
 
-                    <div class="col-10">
+                    <div class="col-xs-7 col-sm-7 col-md-9 col-lg-10">
                         <a href="index.php" id="logo">CMS</a>
 
                         <?php
@@ -79,9 +79,8 @@ if (isset($_SESSION['logged_in'])) {
             </div>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            <script src="../assets/js/jquery-3.3.1.min.js"></script>
+            <script src="../assets/js/bootstrap.min.js"></script>
         </body>
     </html>
 
@@ -160,9 +159,8 @@ if (isset($_SESSION['logged_in'])) {
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="../assets/js/jquery-3.3.1.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
     </body>
     </html>
 
